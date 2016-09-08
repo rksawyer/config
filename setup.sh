bebc792008d63d3718eedf2cf817136a742905ea
Richard Sawyer <rksawyer@gmail.com>
2016-09-08 09:11:54 -0400

    Update setup for gdbinit and gitconfig

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 ln -s -i $DIR/.vimrc ~/.vimrc
 ln -s -i $DIR/.tmux.conf ~/.tmux.conf
+ln -s -i $DIR/.gdbinit ~/.gdbinit
+ln -s -i $DIR/.gitconfig ~/.gitconfig
