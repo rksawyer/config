9bb0670b19615c386c9f85273f49d398235eedb7
Richard Sawyer <rksawyer@gmail.com>
2017-05-25 13:25:00 -0400

    Add gitignore_global

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ ln -s -i $DIR/.vimrc ~/.vimrc
 ln -s -i $DIR/.tmux.conf ~/.tmux.conf
 ln -s -i $DIR/.gdbinit ~/.gdbinit
 ln -s -i $DIR/.gitconfig ~/.gitconfig
+ln -s -i $DIR/.gitignore_global ~/.gitignore_global
 ln -s -i $DIR/.bashrc ~/.bashrc
